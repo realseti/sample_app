a31f0065ae93cda859b3b0d083079e250f89253a
realseti <realseti@gmail.com>
2014-02-24 05:08:14 -0800

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe User do
 
 
-  before do
-    @user = User.new(name: "Example User", email: "user@example.com",
-                     password: "foobar", password_confirmation: "foobar")
+  before do 
+  	@user = User.new(name: "Example User", email: "user@example.com", 
+  					password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -19,12 +19,14 @@ describe User do
 
   it { should be_valid }
 
+
+
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
 
-   describe "when email is not present" do
+  describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
   end
@@ -36,7 +38,7 @@ describe User do
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com]
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -58,19 +60,10 @@ describe User do
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
-      user_with_same_email.save
-    end
-
-    it { should_not be_valid }
-  end
-
-  describe "when email address is already taken" do
-    before do
-      user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
-
+ 
     it { should_not be_valid }
   end
 
@@ -107,4 +100,15 @@ describe User do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
 end
